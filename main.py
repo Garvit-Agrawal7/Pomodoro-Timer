0e5a164f3a9268a23823f07d541ca62a3deb79e9
Garvit-Agrawal7 <garvitagrawal75@gmail.com>
2023-05-25 16:09:57 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 1
+checks = ''
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global checks, reps
+    screen.after_cancel(screen_timer)
+    label.config(text='Timer', fg=GREEN)
+    canvas.itemconfig(timer, text="00:00")
+    reps = 1
+    checks = ''
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    if reps % 8 == 0:
+        count_down(LONG_BREAK_MIN * 60)
+        label.config(text='Break', fg=RED)
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_MIN * 60)
+        label.config(text='Break', fg=PINK)
+    else:
+        count_down(WORK_MIN * 60)
+        label.config(text='Work', fg=GREEN)
+    reps += 1
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    global checks, screen_timer
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if len(str(count_sec)) == 1:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        screen_timer = screen.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        if reps % 2 != 0:
+            checks += '✓'
+        checkmark.config(text=checks)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+screen = Tk()
+screen.title("Pomodoro")
+screen.config(pady=50, padx=100, bg=YELLOW)
+
+canvas = Canvas(width=200, height=250, bg=YELLOW, highlightthickness=0)
+tomato = PhotoImage(file='tomato.png')
+canvas.create_image(100, 125, image=tomato)
+timer = canvas.create_text(100, 140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(row=1, column=1)
+
+label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, 'bold'))
+label.grid(row=0, column=1)
+
+start = Button(text='Start', command=start_timer)
+start.grid(row=2, column=0)
+
+reset = Button(text='Reset', command=reset_timer)
+reset.grid(row=2, column=2)
+
+checkmark = Label(fg=GREEN, bg=YELLOW)
+checkmark.grid(row=3, column=1)
+
+
+screen.mainloop()
